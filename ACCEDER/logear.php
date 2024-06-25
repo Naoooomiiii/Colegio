c43ac315429b210b2fd752b7cbf310f90e58a7ae
Naoooomiiii <naomildiaz12@gmail.com>
2024-06-25 18:11:47 -0500

    Mensaje del commit

---
+++ ACCEDER/logear.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+include 'database.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $role = $_POST['role'];
+
+    // Conexión a la base de datos
+    $conn = connectDatabase();
+
+    // Verificación de las credenciales
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND role = ?");
+    $stmt->bind_param("ss", $username, $role);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['username'] = $username;
+            $_SESSION['role'] = $role;
+            header("Location: dashboard.php");
+        } else {
+            echo "Contraseña incorrecta.";
+        }
+    } else {
+        echo "Usuario no encontrado.";
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
